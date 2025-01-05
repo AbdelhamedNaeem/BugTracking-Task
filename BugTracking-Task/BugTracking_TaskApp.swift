eb731e1b0143a028786a64ec641cf8a5e7bfee62
Abdelhamid <abdelhamid@fanzword.com>
2025-01-05 22:24:45 +0200

    initial commit

---
+++ BugTracking-Task/BugTracking_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugTracking_TaskApp.swift
+//  BugTracking-Task
+//
+//  Created by Abdelhamid on 05/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BugTracking_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
